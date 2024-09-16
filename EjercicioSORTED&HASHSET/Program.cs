3ddd59895528ddc45b503aba1602672dd6b0dc74
Bruno Sponton <brunogsponton@hotmail.com>
2024-09-16 19:25:26 -0300

    Agregar archivos de proyecto.

---
+++ EjercicioSORTED&HASHSET/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class EjercicioSORTEDHASHSET
+{
+    static void Main()
+    {
+        // Crea el conjunto ordenado de frases
+        SortedSet<string> frases = new SortedSet<string>();
+        string frase;
+
+        // Solicita frases al usuario hasta que introduzca una vacia
+        do
+        {
+            Console.WriteLine("Introduce una frase (deja vacía para terminar):");
+            frase = Console.ReadLine();
+
+            // Si la frase no esta vacia, comprobar si ya fue introducida
+            if (!string.IsNullOrEmpty(frase))
+            {
+                if (frases.Contains(frase))
+                {
+                    Console.WriteLine($"La frase '{frase}' ya fue introducida.");
+                }
+                else
+                {
+                    frases.Add(frase);
+                    Console.WriteLine($"La frase '{frase}' ha sido añadida.");
+                }
+            }
+
+        } while (!string.IsNullOrEmpty(frase)); // Repite hasta que la frase este vacia
+
+        Console.WriteLine("Programa finalizado.");
+    }
+}
+
